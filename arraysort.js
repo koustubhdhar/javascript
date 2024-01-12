bb73e9b77ddad5835c0c21a6faca030db45c2dec
Koustubhdhar Damodar Kulkarni <85236083+koustubhdhar@users.noreply.github.com>
2024-01-13 00:02:21 +0530

    Add files via upload

---
+++ arraysort.js
@@ -0,0 +1,8 @@
+const sports = ["cricket","football","competitive-programming"];
+document.write('sports=', sports +"<br>");
+ 
+const myArray = [];
+document.write('myArray=', myArray +"<br>");
+ 
+const score = [420,10,1,12,102];
+document.write('score=', score +"<br>");
